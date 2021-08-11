265f0ff0e1d136d753977391be488b4b7699ac4a
Apoorva <apoorvamandhare2001@gmail.com>
2021-08-11 19:48:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header className="App_header">
+        <h1>Tic Tac Toe</h1>
       </header>
-    </div>
+      <main className="App_main"></main>
+      <footer className="App_footer"></footer>
+    </React.Fragment>
   );
 }
+
 
 export default App;
